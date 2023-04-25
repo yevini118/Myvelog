e067fe47c0095ad3cb748bf39455e140e42b916b
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-04-25 16:56:27 +0900

    Create spring boot project

---
+++ src/main/java/com/yevini/myvelog/MyvelogApplication.java
@@ -0,0 +1,13 @@
+package com.yevini.myvelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yevini/myvelog/MyvelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yevini.myvelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
