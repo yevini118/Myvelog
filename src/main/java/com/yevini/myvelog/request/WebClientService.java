9ef10d7a28ea4abd7086db9c675555d921a0388d
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-05-10 15:34:03 +0900

    Update RequestSpeedTest

--- src/main/java/com/yevini/myvelog/request/WebClientService.java
+++ src/main/java/com/yevini/myvelog/request/WebClientService.java
@@ -4,18 +4,22 @@ import com.yevini.myvelog.request.body.RequestBody;
 import com.yevini.myvelog.request.body.variables.PostsVariables;
 import com.yevini.myvelog.request.body.variables.StatsVariables;
 import com.yevini.myvelog.request.body.variables.UserTagsVariables;
+import com.yevini.myvelog.request.body.variables.Variables;
 import com.yevini.myvelog.response.Post;
 import com.yevini.myvelog.response.Posts;
 import com.yevini.myvelog.response.Stat;
 import com.yevini.myvelog.response.UserTags;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class WebClientService {
@@ -24,7 +28,7 @@ public class WebClientService {
 
     public UserTags getUserTags(String username){
 
-        UserTagsVariables variables = new UserTagsVariables(username);
+        Variables variables = new UserTagsVariables(username);
 
         RequestBody body = RequestBody.builder()
                 .operationName("UserTags")
@@ -52,9 +56,8 @@ public class WebClientService {
 
     public Posts getPosts(String username, int totalPostsCount) {
 
-        PostsVariables variables = PostsVariables.builder()
+        Variables variables = PostsVariables.builder()
                 .username(username)
-                .tag(null)
                 .limit(totalPostsCount)
                 .build();
 
@@ -82,53 +85,18 @@ public class WebClientService {
                 .block();
     }
 
-    public List<Stat> getStats(List<Post> posts, String accessToken) {
+    public List<Stat> getStats(List<Post> posts, String accessToken){
 
-        List<Stat> stats = new ArrayList<>();
         CountDownLatch countDownLatch = new CountDownLatch(posts.size());
 
+        WebClient webClientMutated = webClient.mutate()
+                .defaultCookie("access_token", accessToken)
+                .build();
+
+        List<Stat> stats = Collections.synchronizedList(new ArrayList<>());
         for (Post post : posts) {
 
-            StatsVariables variables = new StatsVariables(post.getId());
-
-            RequestBody body = RequestBody.builder()
-                    .operationName("GetStats")
-                    .variables(variables)
-                    .query("query GetStats($post_id: ID!) {\n  getStats(post_id: $post_id) {\n    total\n    count_by_day {\n      count\n      day\n      __typename\n    }\n    __typename\n  }\n}\n")
-                    .build();
-
-            webClient.mutate()
-                    .defaultCookie("access_token", accessToken)
-                    .build()
-                    .post()
-                    .bodyValue(body)
-                    .retrieve()
-                    .toEntity(Stat.class)
-                    .subscribe(result -> {
-                        System.out.println(result.getStatusCode());
-                        result.getBody().setId(post.getId());
-                        stats.add(result.getBody());
-                        countDownLatch.countDown();
-                    });
-        }
-
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return stats;
-    }
-
-    public List<Stat> getStatsByBlock(List<Post> posts, String accessToken) {
-
-        List<Stat> stats = new ArrayList<>();
-        CountDownLatch countDownLatch = new CountDownLatch(posts.size());
-
-        for (Post post : posts) {
-
-            StatsVariables variables = new StatsVariables(post.getId());
+            Variables variables = new StatsVariables(post.getId());
 
             RequestBody body = RequestBody.builder()
                     .operationName("GetStats")
@@ -146,16 +114,59 @@ public class WebClientService {
                             """)
                     .build();
 
-            stats.add(webClient.mutate()
-                    .defaultCookie("access_token", accessToken)
-                    .build()
-                    .post()
+            webClientMutated
+                .post()
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(Stat.class)
+                .doOnTerminate(countDownLatch::countDown)
+                .subscribe(result -> {
+                    result.setId(post.getId());
+                    stats.add(result);
+                });
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return stats;
+    }
+
+    public List<Stat> getStatsByBlock(List<Post> posts, String accessToken) {
+
+        WebClient webClientMutated = webClient.mutate()
+                .defaultCookie("access_token", accessToken)
+                .build();
+
+        List<Stat> stats = new ArrayList<>();
+        for (Post post : posts) {
+
+            Variables variables = new StatsVariables(post.getId());
+
+            RequestBody body = RequestBody.builder()
+                    .operationName("GetStats")
+                    .variables(variables)
+                    .query("""
+                            query GetStats($post_id: ID!) {
+                                getStats(post_id: $post_id) {
+                                    total
+                                    count_by_day {
+                                        count
+                                        day
+                                    }
+                                }
+                            }
+                            """)
+                    .build();
+
+            stats.add(webClientMutated.post()
                     .bodyValue(body)
                     .retrieve()
-                    .toEntity(Stat.class)
-                    .block()
-                    .getBody());
-
+                    .bodyToMono(Stat.class)
+                    .block());
         }
 
         return stats;
