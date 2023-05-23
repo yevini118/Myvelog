c39545371e61e698eba4203763fa363c23696d2e
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-05-23 16:05:08 +0900

    Update main

--- src/main/java/com/yevini/myvelog/velog/MyvelogService.java
+++ src/main/java/com/yevini/myvelog/velog/MyvelogService.java
@@ -6,19 +6,19 @@ import com.yevini.myvelog.redis.JwtService;
 import com.yevini.myvelog.redis.StatsRedisService;
 import com.yevini.myvelog.redis.UserRedisService;
 import com.yevini.myvelog.request.WebClientService;
-import com.yevini.myvelog.response.Posts;
-import com.yevini.myvelog.response.Stat;
-import com.yevini.myvelog.response.User;
-import com.yevini.myvelog.response.UserTags;
+import com.yevini.myvelog.response.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -58,16 +58,25 @@ public class MyvelogService {
         statsRedisService.set(username, myvelogStats);
 
         int visitsUp = 0;
+        int likesUp = 0;
+        int postsUp = 0;
         LocalDateTime dateTime = LocalDateTime.now();
         if (statsHistory != null) {
             visitsUp = myvelogStats.getTotalVisits() - statsHistory.getTotalVisits();
             dateTime = statsHistory.getDateTime();
+            likesUp = myvelogStats.getTotalLikes() - statsHistory.getTotalLikes();
+            postsUp = myvelogStats.getTotalPosts() -statsHistory.getTotalPosts();
         }
+
+        System.out.println(stats.get(0).getCountByDays().get(1));
 
         model.addAttribute("user", user);
         model.addAttribute("stats", myvelogStats);
         model.addAttribute("visitsUp", visitsUp);
+        model.addAttribute("likesUp", likesUp);
+        model.addAttribute("postsUp", postsUp);
         model.addAttribute("datetime", dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)));
+        model.addAttribute("new", getPostStatByDate(0, myvelogStats.getTopPosts()));
     }
 
     public void post(String username, Model model) {
@@ -86,6 +95,17 @@ public class MyvelogService {
     public void logout(String username) {
 
         userRedisService.delete(username);
+    }
+
+    private List<PostStat> getPostStatByDate(int index, List<PostStat> postStats) {
+
+        List<PostStat> newPostStats = new ArrayList<>();
+
+        postStats.stream()
+                .filter(postStat -> postStat.getCountByDays().get(index).getCount() > 0)
+                .forEach(postStat -> newPostStats.add(new PostStat(postStat, index)));
+
+        return newPostStats;
     }
 
 }
