c323349ab787f731dba12378b0c3c2c88790c1b6
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-09-24 19:31:22 +0900

    Update MyvelogApplication.java

--- src/main/java/com/yevini/myvelog/MyvelogApplication.java
+++ src/main/java/com/yevini/myvelog/MyvelogApplication.java
@@ -3,9 +3,11 @@ package com.yevini.myvelog;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MyvelogApplication {
 
 	public static void main(String[] args) throws InterruptedException, JsonProcessingException {
